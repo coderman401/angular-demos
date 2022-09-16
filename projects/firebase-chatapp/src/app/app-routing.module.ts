d24e5af406a1f2dc25e817290b544b9a4879c867
Kishan Panchal <kishanjpanchal18@gmail.com>
2022-09-16 16:12:59 +0530

    build issue fixed

--- projects/firebase-chatapp/src/app/app-routing.module.ts
+++ projects/firebase-chatapp/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AuthGuard } from './modules/auth/services/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
-  { path: 'chat', loadChildren: () => import('./modules/chat/chat.module').then(m => m.ChatModule), canActivate: [AuthGuard] },
+  // { path: 'chat', loadChildren: () => import('./modules/chat/chat.module').then(m => m.ChatModule), canActivate: [AuthGuard] },
   { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) }
 ];
 
